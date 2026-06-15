0f5e0c31ecfc35911e552578a6e7d7dbe5370f1b
nancyrathod16-pixel <nancyrathod16@gmail.com>
2026-06-15 12:49:05 +0530

    Change name

--- src/components2/Header.js
+++ src/components2/Header.js
@@ -8,7 +8,7 @@ function Header(){
         <div className="d-flex align-items-center justify-content-between">
             <a href="index.html" className="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt=""/>
-                <span className="d-none d-lg-block">NiceAdmin</span>
+                <span className="d-none d-lg-block">Demo Portal</span>
             </a>
             <i className="bi bi-list toggle-sidebar-btn"></i>
         </div>
